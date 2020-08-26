0dcf3d04efec424247ccc27e45c10b29be2fb141
Natalya <0207natalya@gmail.com>
2020-08-26 12:21:52 +0600

    task2

--- homework4/4.2/gameCode.js
+++ homework4/4.2/gameCode.js
@@ -2,14 +2,13 @@ var event, ok;
 var answers = [];
 
 
-do {//Выводим первый вопрос
+do { //Выводим первый вопрос
     ok = false;
     event = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
-   
+    answers.push(event);
     if (event == -1) {
         break;
-    }
-    else {
+    } else {
         ok = isAnswer(works.a0, event);
     }
 } while (!ok);
@@ -18,10 +17,10 @@ switch (event) {
         do {
             ok = false;
             event = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+            answers.push(event);
             if (event == -1) {
                 break;
-            }
-            else {
+            } else {
                 ok = isAnswer(works.b0, event);
             }
         } while (!ok);
@@ -30,10 +29,10 @@ switch (event) {
                 do {
                     ok = false;
                     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(event);
                     if (event == -1) {
                         break;
-                    }
-                    else {
+                    } else {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
@@ -43,10 +42,10 @@ switch (event) {
                 do {
                     ok = false;
                     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(event);
                     if (event == -1) {
                         break;
-                    }
-                    else {
+                    } else {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
@@ -62,10 +61,10 @@ switch (event) {
         do {
             ok = false;
             event = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+            answers.push(event);
             if (event == -1) {
                 break;
-            }
-            else {
+            } else {
                 ok = isAnswer(works.c0, event);
             }
         } while (!ok);
@@ -74,10 +73,10 @@ switch (event) {
                 do {
                     ok = false;
                     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(event);
                     if (event == -1) {
                         break;
-                    }
-                    else {
+                    } else {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
@@ -87,10 +86,10 @@ switch (event) {
                 do {
                     ok = false;
                     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(event);
                     if (event == -1) {
                         break;
-                    }
-                    else {
+                    } else {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
@@ -109,17 +108,57 @@ switch (event) {
 }
 alert('Спасибо за игру');
 
+
+do {
+    lookAnswers = +prompt("Вы хотите просмотреть ответы на вопросы? 1 = да, 0 = нет");
+    if (lookAnswers == 1) {
+        var n = parseInt(prompt('Введите номер вопроса'));
+        if (n == 1) {
+            if (answers[0] == 1) {
+                alert(works.a00 + works.a1);
+            } else {
+                alert(works.a00 + works.a2)
+            };
+        };
+        if (n == 2) {
+            if (answers[1] == 1) {
+                alert(works.b00 + works.b1);
+            } else {
+                alert(works.b00 + works.b2)
+            };
+        };
+        if (n == 3) {
+            if (answers[2] == 1) {
+                alert(works.c00 + works.c1);
+            } else {
+                alert(works.c00 + works.c2)
+            };
+        };
+    }
+else {
+    alert("До свидания");
+}
+}
+while (lookAnswers == 1);
+
+
+
+
+//number(n);
+
+//    function number(n){
+// alert('n')
+//     }
+
 //------------------------------------------
 function isAnswer(q, event) {
     if (isNaN(event) || !isFinite(event)) {
         alert('Вы ввели недопустимый символ');
         return false;
-    }
-    else if (event < 1 || event > q) {
+    } else if (event < 1 || event > q) {
         alert('Ваше число выходит из допустимого диапозона');
         return false;
     }
-	return true;
-    
-}
+    return true;
 
+}
